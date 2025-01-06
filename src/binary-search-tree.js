2ed358b3c41e9a048c822bc8863480b3b1f57e0e
barodzich aliaksandr <barodzichaliaksandr@gmail.com>
2025-01-06 18:51:20 +0300

    feat: implement add method

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { Node } = require('../extensions/list-tree.js');
+const { Node } = require('../extensions/list-tree.js');
 
 /**
 * Implement simple binary search tree according to task description
@@ -19,9 +19,31 @@ class BinarySearchTree {
     left: 'left',
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  #selectDirection(data) {
+    return this.head.data > data ?
+      this.#direction.left :
+      this.#direction.right;
+  }
+
+  #insertInHeadByDirection(data) {
+    const direction = this.#selectDirection(data);
+
+    if (this.head[direction] === null) {
+      this.head[direction] = new Node(data);
+      this.head = this.#root;
+    } else {
+      this.head = this.head[direction];
+      this.add(data);
+    }
+  }
+
+  add(data) {
+    if (this.root() === null) {
+      this.#root = new Node(data);
+    } else {
+      this.#insertInHeadByDirection(data);
+    }
+    this.head = this.#root;
   }
 
   has(/* data */) {
